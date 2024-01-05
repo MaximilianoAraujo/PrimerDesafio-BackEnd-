1280c9c9c47bd1d7e5c6dcd63cf98d56c88a01f3
MaximilianoAraujo <araujo.maximiliano@live.com.ar>
2024-01-05 15:22:10 -0300

    Tercer Desafio Backend. Se implementó un server básico con express que contiene 2 endpoints.

--- src/files/productManager.js
+++ src/files/productManager.js
@@ -1,6 +1,6 @@
-const fs = require('fs')
+import fs from 'fs'
 
-class ProductManager {
+export class ProductManager {
     constructor(path){
         this.path = path;
     }
@@ -21,6 +21,7 @@ class ProductManager {
         }
     }
 
+    // Método para agregar un nuevo producto al archivo products.json. Se valida si todos los campos fueron introducidos. Se valida que no se puedan crear productos con un mismo prodID.
     async addProduct(title, description, price, thumbnail, code, stock) {
         try {
             await this.ensureFileExists();
@@ -69,6 +70,7 @@ class ProductManager {
         
     }
 
+    // Metodo para obtener la lista de productos del archivo products.json
     async getProducts() {
         try {
             await this.ensureFileExists();
@@ -76,14 +78,15 @@ class ProductManager {
             const listProducts = await fs.promises.readFile(this.path, 'utf-8');
             const listProductsToJson = JSON.parse(listProducts);
 
-            console.log('Lista de productos: ');
-            console.log(listProductsToJson);
-            console.log('--------------------');
+            console.log('Lista mostrada en el navegador ');
+
+            return listProductsToJson;
         } catch (error) {
             console.log(error.message);
         }
     }
 
+    // Metodo para obtener un producto en específico de la lista de productos del archivo products.json. Se usa el ID para poder buscar el producto y se valida si el mismo es escontrado o no.
     async getProductById(id) {
         try {
             await this.ensureFileExists();
@@ -96,6 +99,8 @@ class ProductManager {
             if (prodFound) {
                 console.log(prodFound)
                 console.log('--------------------');
+
+                return prodFound;
             } else {
                 console.log('No se encontró el producto');
                 console.log('--------------------');
@@ -105,6 +110,7 @@ class ProductManager {
         }
     }
 
+    // Método para actualizar cualquiera de los valores de los productos. Se utiliza el ID para buscar el producto. Se valida si el producto buscado es encontrado o no.
     async updateProduct(id, updatedFields) {
         try {
             await this.ensureFileExists();
@@ -130,6 +136,7 @@ class ProductManager {
         }
     }
 
+    // Método para elimitar un producto de la lista. Se uutiliza el ID para buscar el producto. Se valida si el producto a eliminar es encontrado o no.
     async deleteProduct(id) {
         try {
             await this.ensureFileExists();
@@ -153,44 +160,3 @@ class ProductManager {
         }
     }
 }
-
-
-const testingProcess = async () => {
-    try {
-        const productos = new ProductManager("./products.json");
-        await productos.getProducts();
-        await productos.addProduct("producto prueba", "Este es un producto prueba", 200, "Sin imagen", "abc123", 25);
-        await productos.addProduct("producto prueba2", "Este es un producto prueba2", 200, "Sin imagen", "abc123", 25);
-        await productos.addProduct("producto prueba3", "Este es un producto prueba3", 200, "Sin imagen", "abc1234", 25);
-        await productos.getProducts();
-        await productos.getProductById(1);
-        await productos.updateProduct(1, { price: 5000 });
-        await productos.deleteProduct(1);
-    } catch (error) {
-        console.log(error.message);
-    }
-}
-
-testingProcess();
-//Proceso de Testing
-// Se creará una instancia de la clase “ProductManager” ✅
-
-// Se llamará “getProducts” recién creada la instancia, debe devolver un arreglo vacío [] ✅
-
-// Se llamará al método “addProduct” con los campos:✅
-// title: “producto prueba”
-// description:”Este es un producto prueba”
-// price:200,
-// thumbnail:”Sin imagen”
-// code:”abc123”,
-// stock:25
-
-// El objeto debe agregarse satisfactoriamente con un id generado automáticamente SIN REPETIRSE ✅
-
-// Se llamará el método “getProducts” nuevamente, esta vez debe aparecer el producto recién agregado ✅
-
-// Se llamará al método “getProductById” y se corroborará que devuelva el producto con el id especificado, en caso de no existir, debe arrojar un error.✅
-
-// Se llamará al método “updateProduct” y se intentará cambiar un campo de algún producto, se evaluará que no se elimine el id y que sí se haya hecho la actualización.✅
-
-// Se llamará al método “deleteProduct”, se evaluará que realmente se elimine el producto o que arroje un error en caso de no existir.✅
